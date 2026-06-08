8bfe66b4713220947b04f27afc9f8e46d40ab8f7
Caetani <bcaetani.poa@gmail.com>
2026-06-08 00:04:47 -0300

    Generating first neural network results

---
+++ src/models/neural_network/neural_network_mlp.py
@@ -0,0 +1,129 @@
+import sys
+sys.path.insert(0, ".")
+
+import joblib
+import os
+import numpy as np
+import pandas as pd
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import PowerTransformer
+from sklearn.decomposition import PCA
+from sklearn.multioutput import MultiOutputRegressor
+import gc
+import matplotlib.pyplot as plt
+from itertools import product
+
+from src.models.dataset_definitions import X_cols, y_cols
+from src.utils.model_evaluation_utils import (
+    evaluate,
+    plot_euler_hist,
+    plot_accuracy_threshold,
+    plot_hist_geodesic,
+    quaternion_to_euler,
+    geodesic_rmse_scorer,
+    geodesic_rmse_oob,
+)
+
+
+if __name__ == '__main__':
+    K_FOLDS = 10
+    OBJECT_IDS = [4] #list(np.arange(1, 16, 1))
+    SPLIT = '70_30'
+    
+    PARAMS = {
+        'loss': 'squared_error',
+        'activation': 'relu',
+        'solver': 'adam',
+        'batch_size': 32,
+        'max_iter': 10_000,
+        #'early_stopping': True,
+        #'validation_fraction': 0.1,
+        #'n_iter_no_change': 10,
+    }
+
+    #LAYERS = [(j,) for j in range(50, 150+10, 10)] + [(i, i) for i in range(50, 150+10, 10)]
+    #ALPHAS = [10**i for i in range(-6, -1, 1)]
+    layers_range = range(50, 550+100, 100)
+    LAYERS = [(j,) for j in layers_range] + [(i, i) for i in layers_range] + [(i, i, i) for i in layers_range]
+    ALPHAS = [10**i for i in range(-6, -1, 1)]
+
+    PARAM_GRID = {
+        'hidden_layer_sizes': LAYERS,
+        'alpha': ALPHAS,
+        #'max_iter': [i for i in range(100, 300+50, 50)]
+    }
+    
+    original_df = pd.read_excel(f'processed/splitted_train_{SPLIT}.xlsx')
+    original_df = original_df[(original_df['frame_id'] != 1277) & (original_df['frame_id'] != 1295)] # Gimbal lock (Pitch = 90% - Yaw == Roll)
+
+    for OBJECT_ID in OBJECT_IDS:
+        print(f"\n\nSeaching model configuration for object {OBJECT_ID}...")
+
+        MODELS_DIR  = f'test_nn/models/object_{OBJECT_ID}/neural_network_{SPLIT}'
+        OUTPUT_DIR = f'{MODELS_DIR}/performance'
+
+        os.makedirs(MODELS_DIR, exist_ok=True)
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+        df_train = original_df[(original_df['set'] == 'train') & (original_df['object_id'] == OBJECT_ID)]
+        df_test = original_df[(original_df['set'] == 'test') & (original_df['object_id'] == OBJECT_ID)]
+
+        X_train = df_train[X_cols]
+        y_train = df_train[y_cols].values
+
+        X_test = df_test[X_cols]
+        y_test = df_test[y_cols].values
+
+        """ hu_moments_for_pca_cols = [
+            'hu_1', 'hu_2', 'hu_3', 'hu_4'
+        ]
+        power_transformer_hu_pca = PowerTransformer(method='yeo-johnson', standardize=True)
+        X_train[hu_moments_for_pca_cols] = power_transformer_hu_pca.fit_transform(X_train[hu_moments_for_pca_cols])
+        X_test[hu_moments_for_pca_cols] = power_transformer_hu_pca.fit_transform(X_test[hu_moments_for_pca_cols])
+        pca_hu = PCA()
+        X_train[hu_moments_for_pca_cols] = pca_hu.fit_transform(X_train[hu_moments_for_pca_cols])
+        X_test[hu_moments_for_pca_cols] = pca_hu.fit_transform(X_test[hu_moments_for_pca_cols]) """
+
+
+        power_transformer = PowerTransformer(method='yeo-johnson', standardize=True)
+        X_train = power_transformer.fit_transform(X_train)
+        X_test = power_transformer.transform(X_test)
+
+        grid_search = GridSearchCV(
+            estimator=MLPRegressor(**PARAMS),
+            param_grid=PARAM_GRID,
+            cv=K_FOLDS,
+            scoring=geodesic_rmse_scorer,
+            n_jobs=4,
+            verbose=3
+        )
+        grid_search.fit(X_train, y_train)
+
+        cv_results  = pd.DataFrame(grid_search.cv_results_)
+        cv_results.sort_values(by=['rank_test_score'], ascending=True, inplace=True)
+        cv_results.reset_index(inplace=True)
+        print(cv_results)
+        cv_results.to_excel(f'{MODELS_DIR}/grid_search_results.xlsx', index=False)
+
+        best_model = grid_search.best_estimator_
+        joblib.dump(best_model, f'{MODELS_DIR}/model.pkl', compress=3)
+
+        y_train_norm, y_pred_train, errors_train = evaluate(best_model, X_train, y_train, "Training set")
+        y_test_norm,  y_pred_test,  errors_test  = evaluate(best_model, X_test, y_test, "Testing set")
+
+        euler_train_true = np.array([quaternion_to_euler(_q) for _q in y_train_norm])
+        euler_train_pred = np.array([quaternion_to_euler(_q) for _q in y_pred_train])
+        euler_test_true  = np.array([quaternion_to_euler(_q) for _q in y_test_norm])
+        euler_test_pred  = np.array([quaternion_to_euler(_q) for _q in y_pred_test])
+
+        plot_euler_hist(euler_train_true, euler_train_pred, 'Train', 'train', OUTPUT_DIR)
+        plot_euler_hist(euler_test_true, euler_test_pred, 'Test', 'test', OUTPUT_DIR)
+        plot_accuracy_threshold(errors_train, errors_test, OUTPUT_DIR)
+        plot_hist_geodesic(errors_train, OUTPUT_DIR, "train", "Treinamento")
+        plot_hist_geodesic(errors_test, OUTPUT_DIR, "test", "Teste")
+
+        plt.close('all')
+        gc.collect()
+
+    print(f"End of Execution.")
